bc4684229d88de025aee5fdbb1f93b8e9c6e8f49
Rashidat <rawsheedah@yahoo.com>
2024-08-31 23:03:07 +0100

    Add Repository layer that performs the CRUD database operation

---
+++ src/main/java/com/example/restapitodolist/todos/TODORepository.java
@@ -0,0 +1,15 @@
+package com.example.restapitodolist.todos;
+
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** 
+ * Interface that extends ListCrudRepository:
+ * Access logic from service, and interact with the database,
+ * using ListCrudRepository which returns a list.
+ */ 
+
+@Repository
+public interface TODORepository extends ListCrudRepository<TODO, Long> {
+    
+}
