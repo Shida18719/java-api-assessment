19e8d586c147791140f588402a6dca3ef61b63c2
Rashidat <rawsheedah@yahoo.com>
2024-08-31 22:54:28 +0100

    Add TODO - Model Entity class and Table that speaks with the database

---
+++ src/main/java/com/example/restapitodolist/todos/TODO.java
@@ -0,0 +1,61 @@
+package com.example.restapitodolist.todos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Model that speaks to the database
+@Entity
+@Table(name = "todos")
+
+public class TODO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean taskCompleted;
+
+    // constructors
+    public TODO() {
+        this(null, null, false);
+    }
+
+    public TODO(String title, String description, boolean taskCompleted) {
+        this.title = title;
+        this.description = description;
+        this.taskCompleted = taskCompleted;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public boolean getTaskCompleted() {
+        return taskCompleted;
+    }
+
+    public void setTaskCompleted(boolean taskCompleted) {
+        this.taskCompleted = taskCompleted;
+    }
+}
