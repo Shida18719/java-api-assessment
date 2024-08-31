ddaa501ff1b3546f4958e01e3b583d71c923a7ef
Rashidat <rawsheedah@yahoo.com>
2024-08-31 22:26:42 +0100

    Initialise spring boot and configure application.properties

---
+++ src/main/java/com/example/restapitodolist/RestapitodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapitodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapitodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapitodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapitodolist/RestapitodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapitodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapitodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
